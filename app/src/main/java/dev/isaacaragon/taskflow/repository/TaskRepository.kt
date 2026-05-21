b67d7fff9c4d728189798a1e39a939be33b499ba
Sofia <165695705+Sooooff@users.noreply.github.com>
2026-05-21 16:02:08 -0600

    feat(TaskRepository)

--- app/src/main/java/dev/isaacaragon/taskflow/repository/TaskRepository.kt
+++ app/src/main/java/dev/isaacaragon/taskflow/repository/TaskRepository.kt
@@ -6,11 +6,11 @@ import dev.isaacaragon.taskflow.model.Estado
 
 class RepositorioTareas {
     private val tareas = mutableListOf<Tarea>(
-        Tarea(1, "Tarea 1", Prioridad.BAJO, Estado.PENDIENTE, false),
-        Tarea(2, "Tarea 2", Prioridad.MEDIO, Estado.COMPLETADA, true),
-        Tarea(3, "Tarea 3", Prioridad.ALTO, Estado.EN_PROGRESO, false),
-        Tarea(4, "Tarea 4", Prioridad.BAJO, Estado.COMPLETADA, true),
-        Tarea(5, "Tarea 5", Prioridad.MEDIO, Estado.PENDIENTE, false)
+        Tarea(1, "Tarea Analisis", Prioridad.BAJO, Estado.PENDIENTE, false),
+        Tarea(2, "Tarea Base de Datos", Prioridad.MEDIO, Estado.COMPLETADA, true),
+        Tarea(3, "Tarea POO II", Prioridad.ALTO, Estado.EN_PROGRESO, false),
+        Tarea(4, "Tarea Foro Ingles", Prioridad.BAJO, Estado.COMPLETADA, true),
+        Tarea(5, "Tarea Redes", Prioridad.MEDIO, Estado.PENDIENTE, false)
     )
 
     fun obtenerTareas(): List<Tarea>  = tareas
